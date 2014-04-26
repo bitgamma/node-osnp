06de72f9eee16b39f7f8f5df41a99fdc621a3a31
Michele Balistreri <michele@briksoftware.com>
2014-04-26 16:41:04 +0200

    added IEEE 802.15.4 frame parser

---
+++ test/osnp.js
@@ -0,0 +1,55 @@
+describe('OSNP', function() {
+  var chai = require('chai');
+  var osnp = require('../lib/osnp.js');
+  chai.should();
+  
+  describe('#parseFrame', function() {    
+    it('should parse a frame and return an OSNPFrame object', function() {
+      var frame = new Buffer([0x61, 0x88, 0x01, 0xfe, 0xca, 0xef, 0xbe, 0xbe, 0xba, 0x74, 0x65, 0x73, 0x74, 0x00, 0x00]);
+      var osnpFrame = osnp.parseFrame(frame);
+      osnpFrame.frameControlLow.should.equal(0x61);
+      osnpFrame.frameControlHigh.should.equal(0x88);
+      osnpFrame.sequenceNumber.should.equal(0x01);
+      osnpFrame.destinationPAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.destinationAddress.should.deep.equal(new Buffer([0xef, 0xbe]));
+      osnpFrame.sourcePAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.sourceAddress.should.deep.equal(new Buffer([0xbe, 0xba]));
+      osnpFrame.payload.should.deep.equal(new Buffer([0x74, 0x65, 0x73, 0x74]));
+    });
+    
+    it('should parse a frame with security but in 802.15.4-2003 format and return an OSNPFrame object', function() {
+      var frame = new Buffer([0x69, 0x88, 0x01, 0xfe, 0xca, 0xef, 0xbe, 0xbe, 0xba, 0x74, 0x65, 0x73, 0x74, 0x00, 0x00]);
+      var osnpFrame = osnp.parseFrame(frame);
+      osnpFrame.frameControlLow.should.equal(0x69);
+      osnpFrame.frameControlHigh.should.equal(0x88);
+      osnpFrame.hasSecurityEnabled().should.equal(true);
+      osnpFrame.hasSecurityHeader().should.equal(false);
+      osnpFrame.sequenceNumber.should.equal(0x01);
+      osnpFrame.destinationPAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.destinationAddress.should.deep.equal(new Buffer([0xef, 0xbe]));
+      osnpFrame.sourcePAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.sourceAddress.should.deep.equal(new Buffer([0xbe, 0xba]));
+      osnpFrame.payload.should.deep.equal(new Buffer([0x74, 0x65, 0x73, 0x74]));
+    });
+    
+    it('should parse a frame with security header and return an OSNPFrame object', function() {
+      var frame = new Buffer([0x69, 0x98, 0x01, 0xfe, 0xca, 0xef, 0xbe, 0xbe, 0xba, 0x15, 0xa7, 0xfa, 0xaf, 0x7a, 0xaa, 0xaa, 0xaa, 0xaa, 0x01, 0x74, 0x65, 0x73, 0x74, 0x00, 0x00]);
+      var osnpFrame = osnp.parseFrame(frame);
+      osnpFrame.frameControlLow.should.equal(0x69);
+      osnpFrame.frameControlHigh.should.equal(0x98);
+      osnpFrame.hasSecurityEnabled().should.equal(true);
+      osnpFrame.hasSecurityHeader().should.equal(true);
+      osnpFrame.sequenceNumber.should.equal(0x01);
+      osnpFrame.destinationPAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.destinationAddress.should.deep.equal(new Buffer([0xef, 0xbe]));
+      osnpFrame.sourcePAN.should.deep.equal(new Buffer([0xfe, 0xca]));
+      osnpFrame.sourceAddress.should.deep.equal(new Buffer([0xbe, 0xba]));
+      osnpFrame.sourceAddress.should.deep.equal(new Buffer([0xbe, 0xba]));
+      osnpFrame.securityControl.should.equal(0x15);
+      osnpFrame.frameCounter.should.equal(0x7aaffaa7);
+      osnpFrame.keySource.should.deep.equal(new Buffer([0xaa, 0xaa, 0xaa, 0xaa]));
+      osnpFrame.keyIndex.should.equal(0x01);
+      osnpFrame.payload.should.deep.equal(new Buffer([0x74, 0x65, 0x73, 0x74]));
+    });
+  });
+});
